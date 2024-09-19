f94c6178309e885c8fbafb5838f0b29b8f3221d3
Karol4TI <karolkort1@wp.pl>
2024-09-19 15:53:56 +0200

    commit 2

--- biuro.php
+++ biuro.php
@@ -2,11 +2,10 @@
 <html lang="pl">
 
 <head>
-    <meta charset="UTF-8" <title>Poznaj Europę</title>
+    <meta charset="UTF-8" 
+    <title>Biuro podróży</title>
     <link rel="stylesheet" href="styl9.css" />
 </head>
-
-<body>
     <header>
         <h1>Biuro podróży</h1>
     </header>
@@ -15,7 +14,7 @@
         <li><a href=>W tym roku jedziemy do...</a></li>
         <li><a href=> Kontakt</a></li>
     </ul>
-    <main>
+    <>
         <section>
             <h2>Promocje</h2>
             <table="1">
@@ -38,8 +37,19 @@
                 </tr>    
                 </table>
         </section>    
-        
-        <h3></h3>
-</body>
-
+        <section>
+        <h2>Oferty</h2>
+        <p>W tym roku pojedziemy do...</p>
+        </section
+         <section>
+            <h2>Kontakt</h2>
+            <p>Napisz do nas: <a href="biuro@wycieczki.pl">biuro@wycieczki.pl</a>napisz do nas!</p>
+            <p>telefon: 444555666</p>
+         </section>
+         <section>
+            <h3>W poprzednich latach byliśmy...</h3>
+            <p>Strone wykonał: Karol Kort 4Ti</p>
+         </section>
+</main>
+  </body>
 </html>
