90476e7735a443d5df97a0a1f5e7da225c262d17
Karol4TI <karolkort1@wp.pl>
2024-09-19 15:09:00 +0200

    projekt biuro

---
+++ biuro.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8" <title>Poznaj Europę</title>
+    <link rel="stylesheet" href="styl9.css" />
+</head>
+
+<body>
+    <header>
+        <h1>Biuro podróży</h1>
+    </header>
+    <ul>
+        <li><a href=>Promocje</a></li>
+        <li><a href=>W tym roku jedziemy do...</a></li>
+        <li><a href=> Kontakt</a></li>
+    </ul>
+    <main>
+        <section>
+            <h2>Promocje</h2>
+            <table="1">
+                <tr>
+                    <th>Miasto</th>
+                </tr>
+                <tr>
+                    <th>Cena</th>
+                </tr>
+                <>
+                    <td>Warszawa</td>
+                    <td> od 600 zł</td>
+                <tr>
+                    <td>Wenecja</td>
+                    <td>od 1200 zł</td>
+                </tr>
+                <tr>
+                    <td>Paryż</td>
+                    <td>od 1200 zł</td>
+                </tr>    
+                </table>
+        </section>    
+        
+        <h3></h3>
+</body>
+
+</html>
