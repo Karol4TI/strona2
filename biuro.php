6873a636b3fec15e84663b48431e5b2a0ecdc018
PC_KAROL\KortK <karolkort1@wp.pl>
2024-10-02 17:29:27 +0200

    Dodanie obrazków i wszystkich wymaganych napisów na stronie oraz nadanie styli

--- biuro.php
+++ biuro.php
@@ -1,32 +1,23 @@
 <!DOCTYPE html>
 <html lang="pl">
-
 <head>
-    <meta charset="UTF-8" 
-    <title>Biuro podróży</title>
-    <link rel="stylesheet" href="styl9.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poznaj Europę</title>
+    <link rel="stylesheet" href="styl9.css">
 </head>
+<body>
     <header>
-        <h1>Biuro podróży</h1>
+        <h1>BIURO PODRÓŻY</h1>
     </header>
-    <ul>
-        <li><a href=>Promocje</a></li>
-        <li><a href=>W tym roku jedziemy do...</a></li>
-        <li><a href=> Kontakt</a></li>
-    </ul>
-    <>
-        <section>
+    <div class="site">
+        <aside class="left">
             <h2>Promocje</h2>
-            <table="1">
+            <table>
                 <tr>
-                    <th>Miasto</th>
-                </tr>
-                <tr>
-                    <th>Cena</th>
-                </tr>
-                <>
                     <td>Warszawa</td>
-                    <td> od 600 zł</td>
+                    <td>od 600 zł</td>
+                </tr>
                 <tr>
                     <td>Wenecja</td>
                     <td>od 1200 zł</td>
@@ -34,22 +25,38 @@
                 <tr>
                     <td>Paryż</td>
                     <td>od 1200 zł</td>
-                </tr>    
-                </table>
-        </section>    
-        <section>
-        <h2>Oferty</h2>
-        <p>W tym roku pojedziemy do...</p>
-        </section
-         <section>
-            <h2>Kontakt</h2>
-            <p>Napisz do nas: <a href="biuro@wycieczki.pl">biuro@wycieczki.pl</a>napisz do nas!</p>
-            <p>telefon: 444555666</p>
-         </section>
-         <section>
-            <h3>W poprzednich latach byliśmy...</h3>
-            <p>Strone wykonał: Karol Kort 4Ti</p>
-         </section>
-</main>
-  </body>
-</html>
+                </tr>
+            </table>
+        </aside>
+        <main>
+            <h2>W tym roku jedziemy do...</h2>
+                <img src="dubai.png" alt="Dubai" width="160" height="110"/>
+                <img src="istambul.png" alt="Istambuł" width="160" height="110"/>
+                <img src="krakow.png" alt="Kraków" width="160" height="110"/>
+               <br> <img src="londyn.png" alt="Londyn" width="160" height="110"/>
+                <img src="madryt.png" alt="Madryt" width="160" height="110"/>
+                <img src="paryz.png" alt="Paryż" width="160" height="110"/>
+               <br> <img src="warszawa.png" alt="Warszawa" width="160" height="110"/>
+                <img src="wenecja.png" alt="Wenecja" width="160" height="110"/>
+                <img src="zagrzeb.png" alt="Zagrzeb" width="160" height="110"/>
+                </main>
+                
+                <aside class="right">
+                    <h2>Kontakt</h2>
+                    <a href=>napisz do nas</a>
+                    <p>telefon: 444555666</p>
+                </aside>
+            </div>
+            <section class="data">
+                <h3>W poprzednich latach byliśmy...</h3>
+                <ol>
+                    <li> Dnia 2019-07-14 pojechaliśmy do Francja, Paryż </li>
+                    <li> Dnia 2019-07-14 pojechaliśmy do Hiszpania, Barcelona </li>
+                    <li> Dnia 2019-08-14 pojechaliśmy do Hiszpania, Barcelona </li>
+                </ol>
+            </section>
+            <footer>
+                <p>Strone wykonał: Karol Kort 4Ti</p>
+            </footer>
+        </body>
+        </html>
